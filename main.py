b606a0710cab4db52eaf9765e76f0a166c4fe6d4
Izel Niyage <izel84@gmail.com>
2024-04-21 18:36:00 -0400

    refactor

--- main.py
+++ main.py
@@ -4,15 +4,8 @@ def main():
     word_count = count_words(text)
     letter_count = count_letters(text)
     letter_count_list = convert_dict_to_list(letter_count)
-    letter_count_list.sort(reverse=True, key=sort_on)
-    #print(text)
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{word_count} words found in the document\n")
-    #print(f"Character count: {letter_count}")  
-    for letter in letter_count_list:
-        print(f"The letter '{letter['char']}' was found {letter['num']} times")
-    print(f"--- End report of {book_path} ---")    
-
+    sorted_letter_count_list = sort_list(letter_count_list)
+    print_report(book_path, word_count, sorted_letter_count_list)
 
 def get_book_text(path):
     with open(path) as f:
@@ -32,13 +25,21 @@ def count_letters(text):
                 letter_counts[char] = 1
     return letter_counts
 
-def sort_on(dict):
-    return dict["num"]
-
 def convert_dict_to_list(dict):
     list = []
     for key in dict:
         list.append({"char": key, "num": dict[key]})
     return list
 
-main()
+def sort_list(list):
+    return sorted(list, key=lambda x: x['num'], reverse=True)
+
+def print_report(book_path, word_count, sorted_letter_count_list):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+    for letter in sorted_letter_count_list:
+        print(f"The letter '{letter['char']}' was found {letter['num']} times")
+    print(f"--- End report of {book_path} ---")
+
+if __name__ == "__main__":
+    main()
